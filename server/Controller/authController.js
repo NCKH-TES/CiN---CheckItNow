12dc50032c2d38ebc6d872d2f730e688ae54e789
Hoang <hoang922001@gmail.com>
2021-10-17 22:56:07 +0700

    sent to cookies

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -25,11 +25,10 @@ exports.login = catchAsync(async (req, res, next) => {
   });
   
   const token = signToken(user.user_id);
-
   const cookieOptions = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: false,
-  }
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: false,
+}
 
   res.cookie('token', token, cookieOptions);
   res.cookie('user_name', user.dataValues.user_name, cookieOptions);
