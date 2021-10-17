414712654c15f2ec4e7cef361fee4cc2921e5094
Hoang <hoang922001@gmail.com>
2021-10-17 21:35:55 +0700

    fix google login

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -23,8 +23,17 @@ exports.login = catchAsync(async (req, res, next) => {
       provider: req.user.provider,
     },
   });
-
+  
   const token = signToken(user.user_id);
+
+  const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: false,
+  }
+
+  res.cookie('token', token, cookieOptions);
+  res.cookie('user_name', user.dataValues.user_name, cookieOptions);
+  
   res.status(200).json({
     status: 'Success',
     user: { ...user.dataValues, token, password: undefined },
