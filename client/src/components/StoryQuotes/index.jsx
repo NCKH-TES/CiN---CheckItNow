03dabe346b572524c722cd88a8b07c4dedadc605
phongduybui <buiduyphong921@gmail.com>
2021-10-18 17:58:07 +0700

    Edit ui by design

--- client/src/components/StoryQuotes/index.jsx
+++ client/src/components/StoryQuotes/index.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { unsplashApi, quotesApi } from '../../services/request';
+import { ReactComponent as ChevronLeft } from '../../assets/icons/chevron-left.svg';
+import { ReactComponent as ChevronRight } from '../../assets/icons/chevron-right.svg';
 import * as S from './styles';
 
 const StoryQuotes = () => {
   const [images, setImages] = useState([]);
-  const [quotes, setQuotes] = useState(null);
+  const [quotes, setQuotes] = useState([]);
+  const [curQuotes, setCurQuotes] = useState(null);
+
+  const handleChangeCurQuote = () => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    setCurQuotes(randomQuote);
+  };
 
   useEffect(() => {
     const getQuotes = async () => {
       const { data } = await quotesApi.get('/');
-      setQuotes(data?.results[0]);
+      setQuotes(data?.results);
+      setCurQuotes(data?.results[0]);
     };
     getQuotes();
   }, []);
@@ -23,21 +32,30 @@ const StoryQuotes = () => {
       let urls = results.map((image) => image.urls.regular);
       setImages(urls);
     };
-    if (quotes) {
-      getImages(quotes?.tags[0]);
+    if (curQuotes) {
+      getImages(curQuotes?.tags[0]);
     }
-  }, [quotes]);
+  }, [curQuotes]);
 
   return (
     <S.StoryWrapper>
       <S.HeaderQuotes>Make your day better</S.HeaderQuotes>
-      <S.Carousel autoplay effect="fade" autoplaySpeed={7000}>
+      <S.Carousel
+        autoplay
+        effect="fade"
+        autoplaySpeed={7000}
+        arrows
+        dots={false}
+        nextArrow={<ChevronRight />}
+        prevArrow={<ChevronLeft />}
+        afterChange={handleChangeCurQuote}
+      >
         {images.map((image) => (
           <S.Image src={image} key={image} alt="" />
         ))}
       </S.Carousel>
-      <S.QuoteContent>{`"${quotes?.quote}"`}</S.QuoteContent>
-      <S.QuoteAuthor> {`- ${quotes?.author}`}</S.QuoteAuthor>
+      <S.QuoteContent>{`"${curQuotes?.quote}"`}</S.QuoteContent>
+      {/* <S.QuoteAuthor> {`- ${quotes?.author}`}</S.QuoteAuthor> */}
     </S.StoryWrapper>
   );
 };
