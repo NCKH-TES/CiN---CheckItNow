291d54cf4c53382e517e89c219add8befe3895df
vanhung <88levanhung.com>
2021-10-23 00:30:47 +0700

    h

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,10 +1,2 @@
 export const SERVER_API = process.env.REACT_APP_SERVER_API;
-<<<<<<< HEAD
 export const APP_ENV = process.env.REACT_APP_ENV;
-=======
-<<<<<<< HEAD
-export const GOOGLE_API = process.env.REACT_APP_GOOGLE;
-=======
-export const APP_ENV = process.env.REACT_APP_ENV;
->>>>>>> b25cfdcc297cffb2c61875f99cb3906f71813bab
->>>>>>> main
