35d512a8cb095e8d909cc07e91a907c14847bd4a
vanhung88 <88levanhung@gmail.com>
2021-12-28 21:22:46 +0700

    fix url,

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -6,7 +6,7 @@ const request = axios.create({
   baseURL:
     APP_ENV === 'development'
       ? SERVER_API
-      : 'http://localhost:5000/api/v1/',
+      : 'https://checkitnowz.herokuapp.com/api/v1/',
   headers: {
     'content-type': 'application/json',
   },
