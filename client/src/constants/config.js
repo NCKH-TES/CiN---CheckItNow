ba0c1703d9576bf45374c1461796cacd8fce75f8
vanhung88 <88levanhung@gmail.com>
2022-03-16 16:29:45 +0700

    fix config

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,2 +1,3 @@
 export const SERVER_API = process.env.REACT_APP_SERVER_API;
+export const SERVER_LOCAL_API = process.env.REACT_APP_SERVER_LOCAL_API;
 export const APP_ENV = process.env.REACT_APP_ENV;
